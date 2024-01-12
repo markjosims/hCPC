eabf8b5af8c9049bf75fdcbd8d3ca14155445009
Mark Simmons <mjsimmons@ucsd.edu>
2024-01-12 08:19:30 -0800

    script makes .csv with frames clipped per wav

--- scripts/detect_audio_clipping.py
+++ scripts/detect_audio_clipping.py
@@ -1,18 +1,29 @@
-"""This script takes one clipped file and declips it. It was created for
-the declipping blog post."""
-# taken from https://github.com/papercup-open-source/tutorials/tree/master/declipping
-# on 7 November 2023
+"""
+Taken from https://github.com/papercup-open-source/tutorials/tree/master/declipping on 7 November 2023.
+Original script declips a single wav file input.
+New script creates a .csv with columns 'filepath' and 'percent_clipped'.
+"""
 from scipy.io.wavfile import read, write
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 from scipy.interpolate import interp1d
+import os
+import pandas as pd
+from typing import List, Tuple, Optional, Sequence
+from tqdm import tqdm
 
-def get_segments(np_array):
+def get_segments(np_array: np.ndarray) -> Tuple[List[Tuple[int, int]], int]:
+    """
+    Given numpy array representing audio samples
+    return a list of tuples containing beginning and end indices of clipped segments,
+    and an integer indicating the percentage of samples which are clipped.
+    """
     nmax = max(np_array)
     nmin = min(np_array)
 
     clipped_segments = []
+    clipped_samples = 0
     inside_clip = False
     clip_start = 0
     clip_end = 0
@@ -27,11 +38,20 @@ def get_segments(np_array):
             inside_clip = False  # not longer inside clipped segment
             clip_end = i-1  # previous sample is end of segment
             clipped_segment = (clip_start, clip_end)  # save segment as tuple
+            clipped_samples += clip_end-clip_start+1 # save number of samples in segment
             clipped_segments.append(clipped_segment)  # store tuple in list of clipped segments
 
-    return clipped_segments
+    percent_clipped = clipped_samples / len(np_array)
+    return clipped_segments, percent_clipped
 
-def declip_segments(clipped_segments, np_array):
+def declip_segments(
+        clipped_segments: List[Tuple[int, int]],
+        np_array: np.ndarray
+    ) -> np.ndarray:
+    """
+    Uses cubic interpolation to declip audio.
+    Returns array same shape as np_array.
+    """
     new_array = np_array.copy()  # make copy of original np_array
     for segment in clipped_segments:
         start = segment[0]
@@ -85,7 +105,8 @@ def save_file(sample_rate, new_array, new_path):
     # save the wav
     write(new_path, sample_rate, new_array)
 
-if __name__ == "__main__":
+def declip_script():
+    # original script
     parser = argparse.ArgumentParser()
     parser.add_argument('--wav_path', required=True, help='Path of the wav file to be declipped')
     parser.add_argument('--new_path', required=True, help='Path to save the new declipped wav file')
@@ -111,3 +132,32 @@ if __name__ == "__main__":
 
     # save new wav file
     save_file(sample_rate, new_array, args.new_path)
+
+def main(argv: Optional[Sequence[str]]=None) -> int:
+    parser = argparse.ArgumentParser(prog='Detect audio clipping')
+    parser.add_argument('WAVS_DIR', help='Dirpath for wav files to perform clipping detection on.')
+    parser.add_argument('OUT_DIR', help='Dirpath to create clipping_data.csv in.')
+    args = parser.parse_args(argv)
+
+    df=pd.DataFrame(columns=['filepath', 'percent_clipped'])
+
+    for dirpath, _, filenames in tqdm(list(os.walk(args.WAVS_DIR))):
+        for f in filenames:
+            if os.path.splitext(f)[1] != '.wav':
+                continue
+            fpath = os.path.join(dirpath, f)
+            _, samples = read(fpath)
+            np_array = np.array(samples, dtype=float)  # load int16 wav file
+            _, percent_clipped = get_segments(np_array)
+            df.loc[len(df)] = {
+                'filepath': fpath,
+                'percent_clipped': percent_clipped,
+            }
+    
+    outpath = os.path.join(args.OUT_DIR, 'clipping_data.csv')
+    df.to_csv(outpath, index=False)
+
+
+
+if __name__ == '__main__':
+    main()
